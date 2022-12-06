803c35645d6f465a071c96c2cde15a0a462282af
MasterOfStar <21274896+MasterOfStar@users.noreply.github.com>
2022-12-06 15:50:46 +0800

    增加了推送优先级自定义选项（未测试）

--- luasrc/model/cbi/pushbot/setting.lua
+++ luasrc/model/cbi/pushbot/setting.lua
@@ -99,8 +99,13 @@ a=s:taboption("basic", Value,"pushdeer_srv",translate('PushDeer Server'), transl
 a.rmempty = true
 a:depends("pushdeer_srv_enable","1")
 
-a=s:taboption("basic", Value,"gotify_token",translate('Gotify Token'), translate("Gotify Token").."<br>调用代码获取<a href='https://gotify.net/api-docs' target='_blank'>点击这里</a><br>默认的优先级为4<br>")
+a=s:taboption("basic", Value,"gotify_token",translate('Gotify Token'), translate("Gotify Token").."<br>调用代码获取<a href='https://gotify.net/api-docs' target='_blank'>点击这里</a>")
 a.rmempty = true
+a:depends("jsonpath","/usr/bin/pushbot/api/gotify.json")
+
+a=s:taboption("basic", Value,"gotify_priority",translate('Gotify priority'), translate("Gotify 通知权重").."<br>默认为4<br>")
+a.rmempty = true
+a.default = "4"
 a:depends("jsonpath","/usr/bin/pushbot/api/gotify.json")
 
 a=s:taboption("basic", Value,"gotify_srv",translate('Gotify Server'), translate("Gotify 自建服务器地址").."<br>如https://your.domain:port<br>注意末尾不要带斜杠<br>具体自建服务器设定参见：<a href='https://gotify.net/' target='_blank'>点击这里</a><br>")
